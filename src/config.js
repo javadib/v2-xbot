29dbccbb24f4751366e8eee13dd0253dbe84c7dd
zill <zilli@zill.io>
2023-09-18 13:40:23 +0330

    R#

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     bot: {
         name: "v2Box-me",
-        token: "6558330560:AAHfBf2CM4VeOE9n_jMeHpXv1lX1OGm8Iio", // Get it from @BotFather https://core.telegram.org/bots#6-botfather
+        token: "6558330560:AAHfBf2CM4VeOE9n_jMeHpXv1lX1OGm8Iio",
         webHook: '/endpoint',
         adminId: 76458757,
         tlgSupport: "@BotFather",
